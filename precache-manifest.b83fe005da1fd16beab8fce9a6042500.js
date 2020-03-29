3b7d4b696e12c9b3122a8d253671aca688026397
Thamal Dilanka <thamaldilanke@gmail.com>
2020-03-30 01:18:10 +0530

    Updates

---
+++ precache-manifest.b83fe005da1fd16beab8fce9a6042500.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7909ca292ea7a920eee47721d87aef6",
+    "url": "/ToDoApp/index.html"
+  },
+  {
+    "revision": "3cf6fead668e1f93f114",
+    "url": "/ToDoApp/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "9921393c7338fd073006",
+    "url": "/ToDoApp/static/css/main.cea84eaf.chunk.css"
+  },
+  {
+    "revision": "3cf6fead668e1f93f114",
+    "url": "/ToDoApp/static/js/2.266d0049.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ToDoApp/static/js/2.266d0049.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9921393c7338fd073006",
+    "url": "/ToDoApp/static/js/main.26c0ef6c.chunk.js"
+  },
+  {
+    "revision": "1b7055c2aba9cc30eb72",
+    "url": "/ToDoApp/static/js/runtime-main.8f7c2a95.js"
+  },
+  {
+    "revision": "9a7e7357f1c3bf9079706e2d4a6f130e",
+    "url": "/ToDoApp/static/media/background.9a7e7357.jpg"
+  },
+  {
+    "revision": "15fcd5db9a49a1af20c3d7b1b80400f2",
+    "url": "/ToDoApp/static/media/placeholder.15fcd5db.jpg"
+  }
+]);
